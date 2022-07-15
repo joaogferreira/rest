9141fe53d9288b695000d24599d05d28b6a5fafd
João Ferreira <jgmpof@gmail.com>
2022-07-15 16:08:06 +0100

    polling command

--- src/main/java/com/network/app/rest/Monitor/Polling.java
+++ src/main/java/com/network/app/rest/Monitor/Polling.java
@@ -3,6 +3,10 @@ package com.network.app.rest.Monitor;
 import com.network.app.rest.Repository.DeviceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Polling extends Thread{
 
     private long interval;
@@ -26,13 +30,26 @@ public class Polling extends Thread{
             System.out.println("Ping " + this.address + " , Time: " + System.currentTimeMillis() / 1000
                 + ", Current db size: " + deviceRepository.count() );
             */
-            
+
+            InetAddress address = null;
+            boolean reachable = false;
+
+            try {
+                address = InetAddress.getByName( this.address );
+                reachable = address.isReachable(1000);
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println( this.address + " - Is host reachable? " + reachable);
+
             try {
                 Thread.sleep( this.interval * 1000 ); // Thread sleep uses milliseconds
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-
 
         }
 
